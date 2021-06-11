f7ce9c53fa35386eb82c42791d653d7c2032fe0d
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-06-11 09:54:59 -0400

    Mx graph again

--- app/frontend/src/components/graphs/boards/infrastructure/svg-board.ts
+++ app/frontend/src/components/graphs/boards/infrastructure/svg-board.ts
@@ -4,7 +4,10 @@ export class SVGBoard extends Board {
 
     constructor(container: HTMLElement) {
         super(container);
-        container.innerHTML += '<svg xmlns="http://www.w3.org/2000/svg" style="z-index:10;margin:0;padding:0;top:0em" onclick="go()" width="100%" height="100%"><circle cx="40" cy="40" r="40" stroke="red" stroke-width="4" fill="blue" /></svg>';
+        const image = require("@/assets/queue-components/router.png");
+        container.innerHTML += '<svg xmlns="http://www.w3.org/2000/svg" style="z-index:10;margin:0;padding:0;top:0em" onclick="go()" width="100%" height="100%">' +
+            '<image href="'+ image +'" height="80" width="80" x="10" y="20" style="fill: red"/>\n' +
+            '</svg>';
     }
 
 
