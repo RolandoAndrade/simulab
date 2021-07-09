ef773a0db9dc556ec971f2e98b188064e1c7f666
rolandoandrade <rolandoandradefernandez@gmail.com>
2021-07-09 09:25:58 -0400

    Starting and ending time

--- app/frontend/src/components/shared/components/layouts/components/AppBar.vue
+++ app/frontend/src/components/shared/components/layouts/components/AppBar.vue
@@ -9,22 +9,76 @@
             <div>Simulation</div>
           </v-col>
         </v-subheader>
-        <tooltip-button tip-color="grey" color="grey" title="Play" outlined>
+        <tooltip-button tip-color="grey" color="grey" title="Play" outlined v-if="!simulationRunning">
           <v-icon color="grey">
             mdi-play
           </v-icon>
         </tooltip-button>
-        <tooltip-button tip-color="grey" color="grey" title="Stop" outlined>
+        <tooltip-button tip-color="grey" color="grey" title="Stop" outlined v-if="simulationRunning">
           <v-icon color="grey">
             mdi-stop
           </v-icon>
         </tooltip-button>
-        <v-col>
-          <v-text-field class="caption mx-1" outlined dense hide-details></v-text-field>
-        </v-col>
-        <v-col>
-          <v-text-field class="caption mx-1" outlined dense hide-details></v-text-field>
-        </v-col>
+        <div class="date-selector">
+          <v-text-field class="caption mx-1" outlined dense hide-details label="Starting time" type="datetime-local"></v-text-field>
+        </div>
+        <div class="date-selector">
+            <v-menu
+                bottom
+                origin="center center"
+                transition="scale-transition"
+                :close-on-content-click="false"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field class="caption mx-1" outlined dense hide-details label="Ending time" v-on="on" readonly></v-text-field>
+              </template>
+              <v-card width="300px" class="pt-2">
+                <v-subheader class="pa-4"><v-icon class="mr-2">mdi-calendar</v-icon>Ending time selection</v-subheader>
+                <v-radio-group v-model="dateSelected" class="ma-0 pa-0">
+                  <v-list>
+                    <v-list-item>
+                      <v-list-item-avatar>
+                        <v-radio :value="true">
+                        </v-radio>
+                      </v-list-item-avatar>
+                      <v-list-item-content>
+                        <v-row no-gutters>
+                          <v-col cols="12">
+                            <v-text-field class="caption mx-1" outlined dense hide-details label="Date" type="datetime-local" :disabled="!dateSelected"></v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-avatar>
+                        <v-radio :value="false">
+                        </v-radio>
+                      </v-list-item-avatar>
+                      <v-list-item-content>
+                        <v-row no-gutters>
+                          <v-col cols="5">
+                            <v-text-field class="caption mx-1" outlined dense hide-details label="Time" type="number" :disabled="dateSelected"></v-text-field>
+                          </v-col>
+                          <v-col cols="7">
+                            <v-select class="caption mx-1" outlined dense hide-details label="Unit" :disabled="dateSelected" :items="[
+                      'Seconds',
+                      'Minutes',
+                      'Hours',
+                      'Days'
+                  ]"></v-select>
+                          </v-col>
+                        </v-row>
+                      </v-list-item-content>
+
+                    </v-list-item>
+                  </v-list>
+                </v-radio-group>
+              </v-card>
+
+
+            </v-menu>
+
+        </div>
       </v-row>
       <v-spacer></v-spacer>
       <tooltip-button title="Save Model" tip-color="grey">
@@ -51,9 +105,16 @@ import TooltipButton from "@/components/shared/components/buttons/TooltipButton.
   components: {TooltipButton},
 })
 export default class AppBar extends Vue {
+  private simulationRunning = false;
 
+  dateSelector = false
+
+  dateSelected = true
 }
 </script>
 
 <style scoped>
+.date-selector{
+  width: 200px;
+}
 </style>
